59b17a7d6f74fe5fd83ed4d7ceb9a6be01f00d31
adamosmanski <62963248+adamosmanski@users.noreply.github.com>
2022-04-01 23:19:49 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enumerable.Range(1, 100).Select(x =>
+            {
+                var str = "";
+                if (x % 3 == 0)
+                {
+                    str += "Fizz";
+                }
+                if (x % 5 == 0)
+                {
+                    str += "Buzz";
+                }
+                if (str == "")
+                {
+                    str += x.ToString();
+                }
+                return str;
+            }).ToList().ForEach(Console.WriteLine);
+        }
+    }
+}
